8130f58cafde88ae11d9e092112cbe8d3e0552e8
antoninabara <60989735+antoninabara@users.noreply.github.com>
2021-08-29 15:04:04 -0700

    game work, snake does not grow

--- snake_game.js
+++ snake_game.js
@@ -8,15 +8,16 @@ const cell_size = container_size/n_cell;
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.getElementById( 'field' );
 
-const x_start = Math.round(Math.random()* n_cell);
-const y_start = Math.round(Math.random()* n_cell);
-const r = 1 * cell_size;
-const step = cell_size;
+let x_snake = Math.round(Math.random()* n_cell);
+let y_snake = Math.round(Math.random()* n_cell);
+const r = 0.5 * cell_size;
+const step = 1;
 const fill_color = "blue";
 const stroke_color = "lightblue";
 
 //x, y - cell numbers from 0 to 60
 function create_circle(x, y, fill_color, stroke_color) {
+    console.log("In create_circl: x=" + x + ", y=" + y);
     const circle = document.createElementNS(svgns, 'circle');
     circle.setAttributeNS(null, 'style', 'fill:' + fill_color + '; stroke:' + stroke_color + '; stroke-width: 1px;' );
     circle.setAttributeNS(null, 'cx', x * cell_size);
@@ -26,43 +27,64 @@ function create_circle(x, y, fill_color, stroke_color) {
 }
 
 // apple
-const x_start_apple = Math.round(Math.random()* n_cell);
-const y_start_apple = Math.round(Math.random()* n_cell);
+let x_start_apple = Math.round(Math.random()* n_cell);
+let y_start_apple = Math.round(Math.random()* n_cell);
 const apple = create_circle(x_start_apple, y_start_apple, "red", "red");
 container.appendChild(apple);
 
 
 
 //snake
-const snake = create_circle(x_start, y_start, "blue", "lightblue");
+const snake = create_circle(x_snake, y_snake, "blue", "lightblue");
 container.appendChild(snake);
 
-let x = x_start * cell_size;
-let y = y_start * cell_size;
+function checkEatApple() {
+    if (x_snake == x_start_apple && y_snake == y_start_apple) {
+        x_start_apple = Math.round(Math.random()* n_cell);
+        y_start_apple = Math.round(Math.random()* n_cell);
+        move_object(apple, x_start_apple, y_start_apple);
+    }
+    console.log("X="+x_snake + ", Y=" + y_start_apple + ", apple X="+ x_start_apple + " apple Y=" + y_start_apple);
+}
+
+//x, y - cell numbers from 0 to 60
+function move_object(circle_obj, x, y) {
+    circle_obj.setAttributeNS(null, 'cx', (x + 0.5) * cell_size);
+    circle_obj.setAttributeNS(null, 'cy', (y + 0.5) * cell_size);
+}
 document.addEventListener('keydown', function(e) {
     switch (e.keyCode) {
         case 37:
            // alert('left');
-            x -= step;
+            x_snake -= step;
+            checkEatApple() ;
             break;
         case 38:
            // alert('up');
-           y -= step;
+           y_snake -= step;
+           checkEatApple();
             break;
         case 39:
-            x += step;
+            x_snake += step;
             // alert('right');
+            checkEatApple();
             break;
         case 40:
-            y += step;
+            y_snake += step;
             // alert('down');
+            checkEatApple();
             break;
     }
-    snake.setAttributeNS(null, 'cx', x);
-    snake.setAttributeNS(null, 'cy', y);
+    move_object(snake, x_snake, y_snake);
 });
 
 
+
+
+
+// function eat_apple() {
+//     if x_start = x_start_apple && y_start = y_start_apple
+// }
 
 
 
