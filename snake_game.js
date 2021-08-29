804dc5caad515a684cd48e8047aca162c151817c
antoninabara <60989735+antoninabara@users.noreply.github.com>
2021-08-29 16:24:34 -0700

    grows, but only for 1

--- snake_game.js
+++ snake_game.js
@@ -8,43 +8,55 @@ const cell_size = container_size/n_cell;
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.getElementById( 'field' );
 
-let x_snake = Math.round(Math.random()* n_cell);
-let y_snake = Math.round(Math.random()* n_cell);
+let x_snake = [Math.round(Math.random()* n_cell)];
+let y_snake = [Math.round(Math.random()* n_cell)];
 const r = 0.5 * cell_size;
+let snake_length = 1;
 const step = 1;
 const fill_color = "blue";
-const stroke_color = "lightblue";
+const stroke_color = "blue";
 
 //x, y - cell numbers from 0 to 60
 function create_circle(x, y, fill_color, stroke_color) {
-    console.log("In create_circl: x=" + x + ", y=" + y);
+   // console.log("In create_circl: x=" + x + ", y=" + y);
     const circle = document.createElementNS(svgns, 'circle');
     circle.setAttributeNS(null, 'style', 'fill:' + fill_color + '; stroke:' + stroke_color + '; stroke-width: 1px;' );
-    circle.setAttributeNS(null, 'cx', x * cell_size);
-    circle.setAttributeNS(null, 'cy', y * cell_size);
+    circle.setAttributeNS(null, 'cx', (x + 0.5) * cell_size);
+    circle.setAttributeNS(null, 'cy', (y + 0.5) * cell_size);
     circle.setAttributeNS(null, 'r', r);
     return circle;
 }
 
 // apple
-let x_start_apple = Math.round(Math.random()* n_cell);
-let y_start_apple = Math.round(Math.random()* n_cell);
-const apple = create_circle(x_start_apple, y_start_apple, "red", "red");
+let x_apple = Math.round(Math.random()* n_cell);
+let y_apple = Math.round(Math.random()* n_cell);
+const apple = create_circle(x_apple, y_apple, "red", "red");
 container.appendChild(apple);
 
 
+// snake
+//const snake_head = create_circle(x_snake, y_snake, "blue", "lightblue");
+function init_snake(x_snake, y_snake) {
+    const snake = [];
+    for (let i = 0; i < x_snake.length; i++) {
+        const snake_circle = create_circle(x_snake[i], y_snake[i], "blue", "lightblue");
+        snake.push(snake_circle);
+        container.appendChild(snake_circle);
+    }
+    return snake;
+}
 
-//snake
-const snake = create_circle(x_snake, y_snake, "blue", "lightblue");
-container.appendChild(snake);
+const snake = init_snake(x_snake, y_snake);
 
 function checkEatApple() {
-    if (x_snake == x_start_apple && y_snake == y_start_apple) {
-        x_start_apple = Math.round(Math.random()* n_cell);
-        y_start_apple = Math.round(Math.random()* n_cell);
-        move_object(apple, x_start_apple, y_start_apple);
+    if (x_snake[0] == x_apple && y_snake[0] == y_apple) {
+        x_apple = Math.round(Math.random()* n_cell);
+        y_apple = Math.round(Math.random()* n_cell);
+        move_object(apple, x_apple, y_apple);
+        return true;
     }
-    console.log("X="+x_snake + ", Y=" + y_start_apple + ", apple X="+ x_start_apple + " apple Y=" + y_start_apple);
+    return false;
+    //console.log("X="+x_snake + ", Y=" + y_start_apple + ", apple X="+ x_start_apple + " apple Y=" + y_start_apple);
 }
 
 //x, y - cell numbers from 0 to 60
@@ -52,30 +64,61 @@ function move_object(circle_obj, x, y) {
     circle_obj.setAttributeNS(null, 'cx', (x + 0.5) * cell_size);
     circle_obj.setAttributeNS(null, 'cy', (y + 0.5) * cell_size);
 }
+function shift_snake(x_snake, y_snake) {
+    const lastCoord = [x_snake[x_snake.length-1], y_snake[y_snake.length-1]];
+    for (let i = 1; i < x_snake.length; i++) {
+        x_snake[i] = x_snake[i-1];
+        y_snake[i] = y_snake[i-1];
+    }
+    return lastCoord;
+}
+
+function move_snake(snake, x_snake, y_snake, needGrow, lastCoord) {
+    //move_object(snake[0], x_snake[0], y_snake[0]);
+    for (i = 0; i < snake.length; i++) {
+        move_object(snake[i], x_snake[i], y_snake[i]);
+    }
+    if (needGrow) {
+        const length = x_snake.length;
+        x_snake[length] = lastCoord[0];
+        y_snake[length] = lastCoord[1];
+        const snake_circle = create_circle(x_snake[length], y_snake[length], "blue", "lightblue");
+        snake.push(snake_circle);
+        container.appendChild(snake_circle);
+    }
+}
+
 document.addEventListener('keydown', function(e) {
+    let needGrow = false;
+    let lastCoord;
     switch (e.keyCode) {
         case 37:
            // alert('left');
-            x_snake -= step;
-            checkEatApple() ;
+           lastCoord = shift_snake(x_snake, y_snake);
+            x_snake[0] -= step;
+            needGrow =checkEatApple() ;
             break;
         case 38:
            // alert('up');
-           y_snake -= step;
-           checkEatApple();
+           lastCoord = shift_snake(x_snake, y_snake);
+           y_snake[0] -= step;
+           needGrow =checkEatApple();
             break;
         case 39:
-            x_snake += step;
+            lastCoord = shift_snake(x_snake, y_snake);
+            x_snake[0] += step;
             // alert('right');
-            checkEatApple();
+            needGrow =checkEatApple();
             break;
         case 40:
-            y_snake += step;
+            lastCoord = shift_snake(x_snake, y_snake);
+            y_snake[0] += step;
             // alert('down');
-            checkEatApple();
+            needGrow =checkEatApple();
             break;
     }
-    move_object(snake, x_snake, y_snake);
+    //move_object(snake[0], x_snake[0], y_snake[0]);
+    move_snake(snake, x_snake, y_snake, needGrow, lastCoord);
 });
 
 
